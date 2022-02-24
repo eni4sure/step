49c79eb6bf6c1db4ad12e43311dc3b4f632a4c42
Eniola Osabiya <eosabiya@gmail.com>
2022-02-24 14:58:19 +0100

    feat: outline edges

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as THREE from "three";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
+import { EffectComposer, Outline } from "@react-three/postprocessing";
 
 import type { NextPage } from "next";
 
@@ -16,32 +17,40 @@ export const IcosahedronGeometryMeshComponent: React.FC = (props) => {
     // Subscribe this component to the render-loop, rotate the mesh every frame
     useFrame((state, delta) => (meshRef.current.rotation.x += 0.01));
 
-    let scale = 1;
-    if (hovered) scale = 1.5;
-    if (clicked) scale = 2;
+    let scale = 2;
+    if (hovered) scale = 2.5;
+    if (clicked) scale = 3;
 
     return (
-        <mesh
-            {...props}
-            ref={meshRef}
-            scale={scale}
-            onClick={(event) => setClicked(!clicked)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}
-        >
-            <icosahedronGeometry attach="geometry" />
-            <meshStandardMaterial attach="material" color={hovered ? "blue" : "white"} />
-        </mesh>
+        <React.Fragment>
+            <mesh
+                {...props}
+                ref={meshRef}
+                scale={scale}
+                onClick={(event) => setClicked(!clicked)}
+                onPointerOver={(event) => setHover(true)}
+                onPointerOut={(event) => setHover(false)}
+            >
+                <icosahedronGeometry attach="geometry" />
+                <meshStandardMaterial attach="material" color={hovered ? "blue" : "white"} />
+            </mesh>
+
+            <EffectComposer autoClear={false}>
+                <Outline blur={true} selection={meshRef} visibleEdgeColor={0x000000} edgeStrength={5} />
+            </EffectComposer>
+        </React.Fragment>
     );
 };
 
 const Home: NextPage = () => {
     return (
         <main className="w-screen h-screen bg-gray-500">
-            <Canvas>
+            <Canvas dpr={[1, 2]}>
                 <Stars />
                 <OrbitControls />
-                <ambientLight intensity={2} />
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
 
                 <IcosahedronGeometryMeshComponent />
             </Canvas>
